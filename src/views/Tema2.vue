33465f02c1a0ea02590539434a7aad2e94aacf29
Jhon Jairo Urueta Alvarez <47787645+jhonuru@users.noreply.github.com>
2022-09-16 10:03:37 -0500

    Avance 1
    Avance.

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,11 +1,45 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Clasificación de ataques
+    .row.align-items-center.mb-5
+      .col-lg.mb-4.mb-lg-0
+        p En la actualidad la amenaza más grande a la que se deben enfrentar las empresas son los ciberataques, teniendo en cuenta que no solo afecta a las empresas, sino también a los individuos particulares e incluso Estados y sociedades.
+        p Por lo tanto, las medidas que se toman en la seguridad informática se han vuelto prioritarias, sobre todo para las organizaciones que dependen casi en su totalidad para funcionar de internet. Las vulnerabilidades en estas aplicaciones y los métodos utilizados para que los atacantes no sean detectados se han convertido en pan de cada día y la práctica de buscar cómo infiltrarse sigue aumentando a diario. Por eso, es necesario mostrar qué clase de ataques existen para ser detectados e identificar su impacto.
+      .col-lg-auto
+        figure
+          img(src='@/assets/curso/tema2/img1.png')
+    p.mb-5 A continuación, en el siguiente video se exponen los ataques más comunes.
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/x56FT_OVARQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .row
+      .col-lg-7.mb-4.mb-lg-0
+        .row.mb-4
+          .col-12.bg-c3.p-4
+            p Para hacer las pruebas es necesario conocer las anteriores terminologías, pues existen diversas técnicas que son utilizadas para atacar una aplicación web y que datos son sensibles a vulnerabilidades; por ello, es necesario contar con una herramienta adecuada para hacer diagnósticos y detectar cada vulnerabilidad.
+            p.mb-0 Por lo anterior, en adelante se encontrará en este componente formativo una prueba a un sitio web, en la que se utilizará una herramienta de detección de vulnerabilidades a una aplicación.
+        .row
+          .col-12.bg-c4.p-4
+            .row.justify-content-center.align-items-center
+              .col-6.col-lg-auto.mb-4.mb-lg-0
+                figure
+                  img(src='@/assets/curso/tema2/img1.svg')
+              .col-lg
+                .row.mb-4
+                  .col-4.bg-c5.p-3
+                    h4.mb-0 Nota:
+                .row
+                  .col-12
+                    p.mb-0 En caso de hacer la práctica, deben tener una autorización previa para no llegar a tener problemas legales, dado que se estará ingresando a zonas de datos privados.
+      .col-lg-5
+        figure
+          img(src='@/assets/curso/tema2/img2.png')
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,11 +1,23 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Auditoría infraestructura tecnológica
+    p.mb-5 Con miras a la ejecución de una prueba de seguridad es necesario observar el video tutorial que se presenta a continuación, el cual brinda los elementos para utilizar la herramienta Owasp ZAP, de manera correcta.
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/VVug58Jaoxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    .row.align-items-center
+      .col-lg.mb-4.mb-lg-0
+        p A continuación, se muestra una forma práctica de identificar vulnerabilidades, que, aunque no se sepa cómo explotarlas, es seguro que hay quien sí lo haga en perjuicio de una persona, o cliente y sus reputaciones. Por razones de seguridad con las empresas testeadas, los ejemplos que se presentarán han sido tomados de alguna página web de una entidad del Estado, de la cual se reservará el nombre y sus datos, además que la imagen gráfica será editada para no comprometer la reputación de la entidad.
+        p.mb-0 Para iniciar, se debe instalar la aplicación Owasp ZAP, en un sistema Windows 10, este aplicativo tiene como propósito hacer pruebas de vulnerabilidad en aplicaciones y tiene como prerrequisito la instalación de JDK de JAVA, por tanto, para realizar estas instalaciones, debe recurrir al material complementario de este componente formativo, específicamente al denominado #[b.txt-c1 “Owasp ZAP, audita la seguridad de webs y evita vulnerabilidades”] (De Luz, 2021), allí se encuentran los enlaces de los manuales, en caso de que no se tengan estas dos aplicaciones en el computador.
+      .col-lg-auto
+        figure
+          img(src='@/assets/curso/tema3/img1.png')
 
 </template>
 
